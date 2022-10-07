e1cb46e6a75c81b7865993d3f9fec5cb6aadb81f
Jamie <nordio.jamie@gmail.com>
2022-10-06 18:23:27 -0700

    add title and styling, fix some js code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -41,25 +41,30 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// function to update player and computer scores
+// function to update player and computer scores and print round result
 function updateScore(winner) {
     if (winner[0] === "Draw") {
-        results.textContent = `The round is a draw. The score is Player ${playerScore} - ${computerScore} Computer.`;
+        results.textContent = `The round is a draw.\r\n
+        The score is Player ${playerScore} - ${computerScore} Computer.`;
     } else if (winner[0] === 'Player') {
         playerScore++;
-        results.textContent = `${winner[0]} wins! ${winner[1]} triumphs over ${winner[2]}. The score is Player ${playerScore} - ${computerScore} Computer.`;
+        results.textContent = `${winner[0]} wins the round!\r\n
+        ${winner[1]} triumphs over ${winner[2]}.\r\n
+        The score is Player ${playerScore} - ${computerScore} Computer.`;
     } else {
         computerScore++;
-        results.textContent = `${winner[0]} wins! ${winner[1]} triumphs over ${winner[2]}. The score is Player ${playerScore} - ${computerScore} Computer.`;
+        results.textContent = `${winner[0]} wins the round!\r\n
+        ${winner[1]} triumphs over ${winner[2]}.\r\n
+        The score is Player ${playerScore} - ${computerScore} Computer.`;
     }
 }
 
 // function to check score between player and computer
 function checkScore() {
     if (playerScore >= 5) {
-        results.textContent = `Player has won the game with 5 wins!`;
+        results.textContent = `Player has won the game with 5 round wins!`;
     } else if (computerScore >= 5) {
-        results.textContent = `Computer has won the game with 5 wins!`;
+        results.textContent = `Computer has won the game with 5 round wins!`;
     }
 }
 
@@ -67,6 +72,7 @@ let computerScore = 0;
 let playerScore = 0;
 
 const results = document.querySelector(".results");
+results.setAttribute('style', 'white-space: pre-line;');
 results.textContent = "The game is ready to begin."
 
 const buttons = document.querySelectorAll('button');
